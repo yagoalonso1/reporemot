ea287bf967c74a85afc665c2ee418c90ccae79ac
yagoalonso1 <123073559+yagoalonso1@users.noreply.github.com>
2023-06-15 11:06:59 +0200

    Primer commit
    He afegit el document i cambiat codi

---
+++ Main.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        menuPrincipal();
+        preuFinal();
+    }
+
+    public static void menuPrincipal (){
+        System.out.println("*******************");
+        System.out.println("\"Hola, benvingut a la calculadora de cost PVP!\"");
+        System.out.println("*******************");
+    }
+
+    public static float llegirFloat() {
+        Scanner llegir = new Scanner(System.in);
+
+        float x = 0;
+        boolean valorCorrecte = false;
+
+        do {
+            System.out.print("Quin és el cost?");
+            valorCorrecte = llegir.hasNextFloat();
+
+            if (!valorCorrecte) {
+                System.out.println("ERROR: Valor no correcte.");
+                llegir.nextLine();
+            } else {
+                x = llegir.nextFloat();
+                llegir.nextLine();
+            }
+
+        } while (!valorCorrecte);
+
+        return x;
+    }
+
+
+    public static float preuFinal (){
+        float preufinalnum = (float) (llegirFloat()*1.21);
+        System.out.println("El preu final és " + preufinalnum + "€");
+        return preufinalnum;
+    }
+}
+
+
+
+
